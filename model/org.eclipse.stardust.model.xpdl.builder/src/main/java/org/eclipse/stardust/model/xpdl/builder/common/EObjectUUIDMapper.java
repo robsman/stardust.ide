5c0a7e6240ca4e80e65cac1d9437d81b35a5f319
Shrikant Gangal <shrikant.gangal@sungard.com>
2012-07-27 13:19:51 +0000

    JIRA Id: CRNT-25579 Moving EObjectUUIDMapper from web-modeler to xpld.builder project
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ide@58013 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

---
+++ model/org.eclipse.stardust.model.xpdl.builder/src/main/java/org/eclipse/stardust/model/xpdl/builder/common/EObjectUUIDMapper.java
@@ -0,0 +1,88 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+
+package org.eclipse.stardust.model.xpdl.builder.common;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
+
+import org.eclipse.emf.ecore.EObject;
+
+/**
+ * @author Shrikant.Gangal
+ * 
+ */
+public class EObjectUUIDMapper
+{
+   /**
+    * 
+    */
+   private Map<UUID, EObject> uuidEObjectMap = new HashMap<UUID, EObject>();
+
+   /**
+    * @param obj
+    */
+   public String map(EObject obj)
+   {
+      UUID uuid = UUID.randomUUID();
+      uuidEObjectMap.put(uuid, obj);
+      
+      return uuid.toString();
+   }
+
+   /**
+    * @param uuid
+    * @return
+    */
+   public EObject getEObject(UUID uuid)
+   {
+
+      return uuidEObjectMap.get(uuid);
+   }
+
+   /**
+    * @param uuid
+    * @return
+    */
+   public EObject getEObject(String uuid)
+   {
+
+      return uuidEObjectMap.get(UUID.fromString(uuid));
+   }
+   
+   /**
+    * @param obj
+    * @return
+    */
+   public String getUUID(EObject obj)
+   {
+      if (null != obj)
+      {
+         Set<Map.Entry<UUID, EObject>> entrySet = uuidEObjectMap.entrySet();
+         for (Map.Entry<UUID, EObject> e : entrySet)
+         {
+            if (obj.equals(e.getValue()))
+            {
+               return e.getKey().toString();
+            }
+         }
+      }
+
+      return null;
+   }
+   
+   public void empty()
+   {
+      uuidEObjectMap = new HashMap<UUID, EObject>();
+   }
+}
