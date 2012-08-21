5a7e4cfdb16d9df45dd50ce92c976578b9f1ebd6
Rainer Pielmann <rainer.pielemann@sungard.com>
2012-08-21 13:37:47 +0000

    Jira-ID: CRNT-25763 Transient Process Instance Support - Modeler functionality: Tagging a process definition
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ide@58587 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- modeling/org.eclipse.stardust.modeling.core/src/org/eclipse/stardust/modeling/core/properties/ProcessDefinitionGeneralPropertyPage.java
+++ modeling/org.eclipse.stardust.modeling.core/src/org/eclipse/stardust/modeling/core/properties/ProcessDefinitionGeneralPropertyPage.java
@@ -10,14 +10,21 @@
  *******************************************************************************/
 package org.eclipse.stardust.modeling.core.properties;
 
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Composite;
+
 import org.eclipse.stardust.common.StringUtils;
+import org.eclipse.stardust.model.xpdl.carnot.AttributeType;
+import org.eclipse.stardust.model.xpdl.carnot.IExtensibleElement;
 import org.eclipse.stardust.model.xpdl.carnot.IModelElement;
 import org.eclipse.stardust.model.xpdl.carnot.IModelElementNodeSymbol;
 import org.eclipse.stardust.model.xpdl.carnot.ProcessDefinitionType;
+import org.eclipse.stardust.model.xpdl.carnot.util.AttributeUtil;
 import org.eclipse.stardust.modeling.common.ui.jface.utils.FormBuilder;
 import org.eclipse.stardust.modeling.common.ui.jface.utils.LabeledText;
 import org.eclipse.stardust.modeling.core.Diagram_Messages;
-import org.eclipse.swt.widgets.Composite;
 
 public class ProcessDefinitionGeneralPropertyPage extends IdentifiablePropertyPage
 {
@@ -25,11 +32,25 @@ public class ProcessDefinitionGeneralPropertyPage extends IdentifiablePropertyPa
    
    private LabeledText priorityText;
 
+   private Button transientCheckBox;
+
+   private boolean isTransient = false;
+
    public void loadFieldsFromElement(IModelElementNodeSymbol symbol, IModelElement element)
    {
       super.loadFieldsFromElement(symbol, element);
       ProcessDefinitionType pd = (ProcessDefinitionType) element;
       priorityText.getText().setText(Integer.toString(pd.getDefaultPriority()));
+      
+      AttributeType transientAttribute = AttributeUtil.getAttribute(
+            (IExtensibleElement) getModelElement(),
+            "carnot:engine:transientProcessExecutionSupport");
+
+      if (transientAttribute != null)
+      {
+         isTransient = AttributeUtil.getBooleanValue(transientAttribute);
+      }
+      transientCheckBox.setSelection(isTransient);
    }
    
    public void loadElementFromFields(IModelElementNodeSymbol symbol, IModelElement element)
@@ -58,5 +79,29 @@ public class ProcessDefinitionGeneralPropertyPage extends IdentifiablePropertyPa
    {
       priorityText = FormBuilder.createLabeledText(composite,
             Diagram_Messages.LBL_TXT_DEFAULT_PRIORITY);
+
+      transientCheckBox = FormBuilder.createCheckBox(composite, "Transient", 2);
+      transientCheckBox.addSelectionListener(new SelectionAdapter()
+      {
+
+         public void widgetSelected(SelectionEvent e)
+         {
+
+            AttributeUtil.setBooleanAttribute((IExtensibleElement) modelElement,
+                  "carnot:engine:transientProcessExecutionSupport", true);
+            isTransient = !isTransient;
+            if (isTransient)
+            {
+               AttributeUtil.setBooleanAttribute((IExtensibleElement) modelElement,
+                     "carnot:engine:transientProcessExecutionSupport", true);
+            }
+            else
+            {
+               AttributeUtil.setBooleanAttribute((IExtensibleElement) modelElement,
+                     "carnot:engine:transientProcessExecutionSupport", false);
+            }
+         }
+      });
+
    }
 }
