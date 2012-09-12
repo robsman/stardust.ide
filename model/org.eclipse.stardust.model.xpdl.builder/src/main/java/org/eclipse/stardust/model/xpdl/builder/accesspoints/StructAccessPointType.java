4bfb2f71b36233f308989df421f41b4bd6edaaea
Rainer Pielmann <rainer.pielemann@sungard.com>
2012-09-12 08:06:41 +0000

    CRNT-26125 Pepper: ModelBuilder API - Create accesspoints for MTA / UI Mashup (Primitive Accesspoints)
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ide@59109 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

---
+++ model/org.eclipse.stardust.model.xpdl.builder/src/main/java/org/eclipse/stardust/model/xpdl/builder/accesspoints/StructAccessPointType.java
@@ -0,0 +1,64 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.stardust.model.xpdl.builder.accesspoints;
+
+import org.eclipse.stardust.engine.core.struct.IXPathMap;
+import org.eclipse.stardust.engine.core.struct.TypedXPath;
+import org.eclipse.stardust.model.xpdl.carnot.impl.AccessPointTypeImpl;
+
+public class StructAccessPointType extends AccessPointTypeImpl
+{
+   private TypedXPath xPath;
+   private IXPathMap xPathMap;
+
+   public StructAccessPointType(TypedXPath xPath, IXPathMap xPathMap)
+   {
+      this.xPath = xPath;
+      this.xPathMap = xPathMap;
+   }
+
+   public TypedXPath getXPath()
+   {
+      return xPath;
+   }
+
+   public IXPathMap getXPathMap()
+   {
+      return xPathMap;
+   }
+
+   public int hashCode()
+   {
+      final int PRIME = 31;
+      int result = 1;
+      result = PRIME * result + ((name == null) ? 0 : name.hashCode());
+      return result;
+   }
+
+   public boolean equals(Object obj)
+   {
+      if (this == obj)
+         return true;
+      if (obj == null)
+         return false;
+      if (getClass() != obj.getClass())
+         return false;
+      final StructAccessPointType other = (StructAccessPointType) obj;
+      if (name == null)
+      {
+         if (other.name != null)
+            return false;
+      }
+      else if (!name.equals(other.name))
+         return false;
+      return true;
+   }
+}
