92e92e558b994553f86768496432681df59b178b
Florin Herinean <florin.herinean@sungard.com>
2013-01-18 10:19:00 +0000

    Jira-ID: CRNT-27643 provided builder to create camel triggers
    git-svn-id: http://emeafrazerg/svn/ipp/product/trunk/stardust/ide@62130 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- model/org.eclipse.stardust.model.xpdl.builder/src/main/java/org/eclipse/stardust/model/xpdl/builder/process/BpmCamelTriggerBuilder.java
+++ model/org.eclipse.stardust.model.xpdl.builder/src/main/java/org/eclipse/stardust/model/xpdl/builder/process/BpmCamelTriggerBuilder.java
@@ -13,6 +13,7 @@ package org.eclipse.stardust.model.xpdl.builder.process;
 import org.eclipse.stardust.model.xpdl.builder.utils.ModelerConstants;
 import org.eclipse.stardust.model.xpdl.builder.utils.XpdlModelUtils;
 import org.eclipse.stardust.model.xpdl.carnot.ProcessDefinitionType;
+import org.eclipse.stardust.model.xpdl.carnot.TriggerType;
 import org.eclipse.stardust.model.xpdl.carnot.TriggerTypeType;
 import org.eclipse.stardust.model.xpdl.carnot.util.AttributeUtil;
 
@@ -22,25 +23,6 @@ public class BpmCamelTriggerBuilder
    private BpmCamelTriggerBuilder(ProcessDefinitionType process)
    {
       super(process);
-
-      TriggerTypeType triggerMetaType = XpdlModelUtils.findIdentifiableElement(
-            model.getTriggerType(), ModelerConstants.CAMEL_TRIGGER_TYPE_ID);
-      if (null == triggerMetaType)
-      {
-         triggerMetaType = F_CWM.createTriggerTypeType();
-         
-         triggerMetaType.setElementOid(XpdlModelUtils.getMaxUsedOid(model) + 1);
-         triggerMetaType.setId(ModelerConstants.CAMEL_TRIGGER_TYPE_ID);
-         triggerMetaType.setName("Camel Trigger");
-         triggerMetaType.setIsPredefined(true);
-         triggerMetaType.setPullTrigger(false);
-         
-         AttributeUtil.setAttribute(triggerMetaType, "carnot:engine:validator", "org.eclipse.stardust.engine.extensions.camel.trigger.validation.CamelTriggerValidator");
-         AttributeUtil.setAttribute(triggerMetaType, "carnot:engine:runtimeValidator", "org.eclipse.stardust.engine.extensions.camel.trigger.validation.CamelTriggerValidator");
-
-         model.getTriggerType().add(triggerMetaType);
-      }
-      element.setType(triggerMetaType);
    }
 
    @Override
@@ -54,13 +36,30 @@ public class BpmCamelTriggerBuilder
       return new BpmCamelTriggerBuilder(process);
    }
 
-   /*@Override
    protected TriggerType finalizeElement()
    {
-      TriggerType result = super.finalizeElement();
+      long maxUsedOid = XpdlModelUtils.getMaxUsedOid(model);
+      TriggerTypeType triggerMetaType = XpdlModelUtils.findIdentifiableElement(
+            model.getTriggerType(), ModelerConstants.CAMEL_TRIGGER_TYPE_ID);
+      if (null == triggerMetaType)
+      {
+         triggerMetaType = F_CWM.createTriggerTypeType();
+         
+         triggerMetaType.setElementOid(++maxUsedOid);
+         triggerMetaType.setId(ModelerConstants.CAMEL_TRIGGER_TYPE_ID);
+         triggerMetaType.setName("Camel Trigger");
+         triggerMetaType.setIsPredefined(true);
+         triggerMetaType.setPullTrigger(false);
+         
+         AttributeUtil.setAttribute(triggerMetaType, "carnot:engine:validator", "org.eclipse.stardust.engine.extensions.camel.trigger.validation.CamelTriggerValidator");
+         AttributeUtil.setAttribute(triggerMetaType, "carnot:engine:runtimeValidator", "org.eclipse.stardust.engine.extensions.camel.trigger.validation.CamelTriggerValidator");
+
+         model.getTriggerType().add(triggerMetaType);
+      }
       
-      // TODO: add extra actions, otherwise delete that method completely.
-      
-      return result;
-   }*/
+      super.finalizeElement();
+      element.setElementOid(++maxUsedOid);
+      element.setType(triggerMetaType);
+      return element;
+   }
 }
