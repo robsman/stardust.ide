0922b07034064a0ecb248939be2992c442bfbf99
Florin Herinean <florin.herinean@sungard.com>
2013-05-23 09:24:55 +0000

    fixed transformation for locking of models
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/branches/b_dev_7_1_x/stardust/ide@65019 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Florin Herinean <florin.herinean@sungard.com>

--- model/org.eclipse.stardust.model.xpdl.builder/src/main/java/org/eclipse/stardust/model/xpdl/builder/utils/MappingModelUtil.java
+++ model/org.eclipse.stardust.model.xpdl.builder/src/main/java/org/eclipse/stardust/model/xpdl/builder/utils/MappingModelUtil.java
@@ -16,57 +16,54 @@ import java.io.IOException;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
-import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.emf.ecore.resource.ResourceSet;
-import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.emf.ecore.util.ExtendedMetaData;
 import org.eclipse.emf.ecore.xmi.XMLResource;
-import org.eclipse.emf.ecore.xmi.impl.XMLResourceFactoryImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMLResourceImpl;
 import org.eclipse.stardust.engine.core.compatibility.ipp.PreStardustTypeNameResolver;
-
+import org.eclipse.stardust.model.xpdl.carnot.ModelType;
 
 public class MappingModelUtil
 {
-   public static String transformEcore2XML(EObject eobject) {
-      ByteArrayOutputStream stream = new ByteArrayOutputStream();
-      ResourceSet resourceSet = new ResourceSetImpl();
-      resourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("xml", new XMLResourceFactoryImpl());
-      Resource resource = resourceSet.createResource(URI.createFileURI("temp.xml"));
-      ((XMLResource)resource).getDefaultSaveOptions().put(XMLResource.OPTION_SCHEMA_LOCATION_IMPLEMENTATION, Boolean.TRUE);
-      resource.getContents().add(eobject);
-      try {
-          resource.save(stream, null);
-          return stream.toString();
-      } catch (IOException e) {
+   public static String transformEcore2XML(ModelType model)
+   {
+      try
+      {
+         ByteArrayOutputStream stream = new ByteArrayOutputStream();
+         WebModelerModelManager mgr = new WebModelerModelManager();
+         mgr.setModel(model);
+         mgr.save(URI.createFileURI("temp.xml"), stream);
+         return stream.toString("UTF8");
+      }
+      catch (IOException e)
+      {
          throw new RuntimeException("Could not complete transformation", e);
       }
-  }
+   }
 
-   public static EObject transformXML2Ecore(String xmlMessage) {
+   public static EObject transformXML2Ecore(String xmlMessage)
+   {
       PreStardustTypeNameResolver typeNameResolver = new PreStardustTypeNameResolver();
       String r1 = typeNameResolver.resolveTypeName("com.infinity.bpm.messaging.model");
       String r2 = typeNameResolver.resolveTypeName("com.infinity.bpm.messaging.model.mapping.MappingPackage");
       xmlMessage = xmlMessage.replaceAll("com.infinity.bpm.messaging.model.mapping.MappingPackage", r2);
       xmlMessage = xmlMessage.replaceAll("com.infinity.bpm.messaging.model", r1);
       ByteArrayInputStream stream = new ByteArrayInputStream(xmlMessage.getBytes());
-      XMLResourceImpl resource=new XMLResourceImpl();
-      resource.getDefaultSaveOptions().put(XMLResource.OPTION_EXTENDED_META_DATA,ExtendedMetaData.INSTANCE);
-      resource.getDefaultLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA,ExtendedMetaData.INSTANCE);
-      resource.getDefaultSaveOptions().put(XMLResource.OPTION_SCHEMA_LOCATION,Boolean.TRUE);
+      XMLResourceImpl resource = new XMLResourceImpl();
+      resource.getDefaultSaveOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, ExtendedMetaData.INSTANCE);
+      resource.getDefaultLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA, ExtendedMetaData.INSTANCE);
+      resource.getDefaultSaveOptions().put(XMLResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
       resource.getDefaultSaveOptions().put(XMLResource.OPTION_USE_ENCODED_ATTRIBUTE_STYLE, Boolean.TRUE);
       resource.getDefaultLoadOptions().put(XMLResource.OPTION_USE_LEXICAL_HANDLER, Boolean.TRUE);
       resource.getDefaultLoadOptions().put(XMLResource.OPTION_SCHEMA_LOCATION_IMPLEMENTATION, Boolean.TRUE);
       resource.getDefaultLoadOptions().put(XMLResource.OPTION_RECORD_UNKNOWN_FEATURE, Boolean.TRUE);
-      try{
-         resource.load(stream,null);
-         return (EObject)resource.getContents().get(0);
-      }catch(Exception e){
+      try
+      {
+         resource.load(stream, null);
+         return (EObject) resource.getContents().get(0);
+      }
+      catch (Exception e)
+      {
          throw new RuntimeException("Could not read message transformation configuration", e);
       }
-  }
-
-
-
-
+   }
 }
