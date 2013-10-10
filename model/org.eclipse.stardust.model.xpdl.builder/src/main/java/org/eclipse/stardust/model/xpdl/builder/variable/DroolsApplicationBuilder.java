df7174e1623be70408ecc6742bde779282c164b3
Rainer Pielmann <rainer.pielmann@sungard.com>
2013-10-10 15:04:09 +0000

    Jira-ID: CRNT-30324
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ide@67903 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

---
+++ model/org.eclipse.stardust.model.xpdl.builder/src/main/java/org/eclipse/stardust/model/xpdl/builder/variable/DroolsApplicationBuilder.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2012 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     SunGard CSA LLC - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.stardust.model.xpdl.builder.variable;
+
+import org.eclipse.stardust.model.xpdl.builder.common.AbstractElementBuilder;
+import org.eclipse.stardust.model.xpdl.builder.common.AbstractModelElementBuilder;
+import org.eclipse.stardust.model.xpdl.builder.utils.ModelerConstants;
+import org.eclipse.stardust.model.xpdl.carnot.ApplicationType;
+import org.eclipse.stardust.model.xpdl.carnot.ApplicationTypeType;
+import org.eclipse.stardust.model.xpdl.carnot.ModelType;
+import org.eclipse.stardust.model.xpdl.carnot.util.ModelUtils;
+
+public class DroolsApplicationBuilder extends AbstractModelElementBuilder<ApplicationType, DroolsApplicationBuilder>
+{
+
+   public DroolsApplicationBuilder(ModelType model)
+   {
+      super(F_CWM.createApplicationType());
+
+      forModel(model);
+
+      ApplicationTypeType applicationMetaType = ModelUtils.findIdentifiableElement(
+            model.getApplicationType(), ModelerConstants.DROOLS_APPLICATION_TYPE_ID);
+      if (null == applicationMetaType)
+      {
+         applicationMetaType = AbstractElementBuilder.F_CWM.createApplicationTypeType();
+         applicationMetaType.setId(ModelerConstants.DROOLS_APPLICATION_TYPE_ID);
+         applicationMetaType.setName("Drools Application");
+         applicationMetaType.setIsPredefined(true);
+         applicationMetaType.setSynchronous(true);
+         /*AttributeUtil.setAttribute(applicationMetaType, "carnot:engine:validator", "org.eclipse.stardust.engine.extensions.camel.app.CamelProducerSpringBeanValidator");
+         AttributeUtil.setAttribute(applicationMetaType, "carnot:engine:accessPointProvider", "org.eclipse.stardust.engine.extensions.camel.app.CamelProducerSpringBeanAccessPointProvider");
+         AttributeUtil.setAttribute(applicationMetaType, "carnot:engine:applicationInstance", "org.eclipse.stardust.engine.extensions.camel.app.CamelProducerSpringBeanApplicationInstance");*/
+         model.getApplicationType().add(applicationMetaType);
+      }
+      element.setType(applicationMetaType);
+   }
+
+   @Override
+   protected String getDefaultElementIdPrefix()
+   {
+      return "Application";
+   }
+
+   public static DroolsApplicationBuilder newDroolsApplication(ModelType model)
+   {
+      return new DroolsApplicationBuilder(model);
+   }
+
+   @Override
+   protected ApplicationType finalizeElement()
+   {
+      super.finalizeElement();
+      model.getApplication().add(element);
+      return element;
+   }
+
+}
