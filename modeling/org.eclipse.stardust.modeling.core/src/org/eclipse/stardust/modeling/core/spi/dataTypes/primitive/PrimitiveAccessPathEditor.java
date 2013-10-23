0e8a623b615ef25348818f97652253f51234a410
Barry Grotjahn <barry.grotjahn@sungard.com>
2013-10-23 12:21:06 +0000

    Jira-ID: CRNT-30867 adapting validation
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ide@68278 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- modeling/org.eclipse.stardust.modeling.core/src/org/eclipse/stardust/modeling/core/spi/dataTypes/primitive/PrimitiveAccessPathEditor.java
+++ modeling/org.eclipse.stardust.modeling.core/src/org/eclipse/stardust/modeling/core/spi/dataTypes/primitive/PrimitiveAccessPathEditor.java
@@ -10,11 +10,22 @@
  *******************************************************************************/
 package org.eclipse.stardust.modeling.core.spi.dataTypes.primitive;
 
+import org.eclipse.xsd.XSDNamedComponent;
+import org.eclipse.xsd.XSDSimpleTypeDefinition;
+
 import org.eclipse.stardust.common.StringUtils;
 import org.eclipse.stardust.common.reflect.Reflect;
+import org.eclipse.stardust.engine.core.pojo.data.Type;
+import org.eclipse.stardust.engine.core.struct.StructuredDataConstants;
 import org.eclipse.stardust.model.xpdl.carnot.IExtensibleElement;
+import org.eclipse.stardust.model.xpdl.carnot.ModelType;
 import org.eclipse.stardust.model.xpdl.carnot.util.AttributeUtil;
 import org.eclipse.stardust.model.xpdl.carnot.util.CarnotConstants;
+import org.eclipse.stardust.model.xpdl.carnot.util.ModelUtils;
+import org.eclipse.stardust.model.xpdl.xpdl2.TypeDeclarationType;
+import org.eclipse.stardust.model.xpdl.xpdl2.TypeDeclarationsType;
+import org.eclipse.stardust.model.xpdl.xpdl2.util.ExtendedAttributeUtil;
+import org.eclipse.stardust.model.xpdl.xpdl2.util.TypeDeclarationUtils;
 import org.eclipse.stardust.modeling.core.spi.dataTypes.serializable.SerializableAccessPathEditor;
 
 /**
@@ -25,8 +36,42 @@ public class PrimitiveAccessPathEditor extends SerializableAccessPathEditor
 {
    protected String getClassName(IExtensibleElement data)
    {
-      String className = AttributeUtil.getAttributeValue(
-            data, CarnotConstants.TYPE_ATT);
+      String className = null;
+      String dataType = AttributeUtil.getAttributeValue((IExtensibleElement) data, CarnotConstants.TYPE_ATT);
+      if(dataType.equals(Type.Enumeration.toString()))
+      {
+         TypeDeclarationType typeDeclaration = null;                                    
+         String typeDeclarationId = AttributeUtil.getAttributeValue((IExtensibleElement) data, StructuredDataConstants.TYPE_DECLARATION_ATT);
+         if(!StringUtils.isEmpty(typeDeclarationId))
+         {
+            ModelType model = ModelUtils.findContainingModel(data);
+            if (model != null)
+            {
+               TypeDeclarationsType declarations = model.getTypeDeclarations();
+               if (declarations != null)
+               {
+                  typeDeclaration = declarations.getTypeDeclaration(typeDeclarationId);
+               }
+            }
+         }
+         if(typeDeclaration != null)
+         {
+            XSDNamedComponent component = TypeDeclarationUtils.getSimpleType(typeDeclaration);
+            if (component instanceof XSDSimpleTypeDefinition)
+            {
+               className = ExtendedAttributeUtil.getAttributeValue(typeDeclaration, CarnotConstants.CLASS_NAME_ATT);            
+               if(!StringUtils.isEmpty(className))
+               {
+                  return className;
+               }               
+            }  
+         }            
+      }
+      else
+      {
+         className = AttributeUtil.getAttributeValue(
+               data, CarnotConstants.TYPE_ATT);         
+      }
       if(StringUtils.isEmpty(className))
       {
          return null;
