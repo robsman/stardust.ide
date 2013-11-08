a0688132a54507a7dee0f1a2a3a439bd0155219f
Florin Herinean <florin.herinean@sungard.com>
2013-11-08 12:39:21 +0000

    Jira-ID: CRNT-30868 Fixed eclipse modeler validation when enumerations are from a different model
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ide@68686 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- modeling/org.eclipse.stardust.modeling.validation/src/org/eclipse/stardust/modeling/validation/impl/spi/dataTypes/PrimitiveValidator.java
+++ modeling/org.eclipse.stardust.modeling.validation/src/org/eclipse/stardust/modeling/validation/impl/spi/dataTypes/PrimitiveValidator.java
@@ -11,13 +11,8 @@
 package org.eclipse.stardust.modeling.validation.impl.spi.dataTypes;
 
 import java.text.MessageFormat;
-import java.util.List;
 
-import org.eclipse.xsd.XSDEnumerationFacet;
-import org.eclipse.xsd.XSDNamedComponent;
-import org.eclipse.xsd.XSDSimpleTypeDefinition;
-
-import org.eclipse.stardust.common.CollectionUtils;
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.stardust.common.StringUtils;
 import org.eclipse.stardust.engine.core.pojo.data.Type;
 import org.eclipse.stardust.engine.core.struct.StructuredDataConstants;
@@ -25,101 +20,71 @@ import org.eclipse.stardust.model.xpdl.carnot.DirectionType;
 import org.eclipse.stardust.model.xpdl.carnot.IExtensibleElement;
 import org.eclipse.stardust.model.xpdl.carnot.IModelElement;
 import org.eclipse.stardust.model.xpdl.carnot.ITypedElement;
-import org.eclipse.stardust.model.xpdl.carnot.ModelType;
 import org.eclipse.stardust.model.xpdl.carnot.util.AttributeUtil;
 import org.eclipse.stardust.model.xpdl.carnot.util.CarnotConstants;
-import org.eclipse.stardust.model.xpdl.carnot.util.ModelUtils;
 import org.eclipse.stardust.model.xpdl.xpdl2.TypeDeclarationType;
-import org.eclipse.stardust.model.xpdl.xpdl2.TypeDeclarationsType;
 import org.eclipse.stardust.model.xpdl.xpdl2.util.TypeDeclarationUtils;
 import org.eclipse.stardust.modeling.validation.*;
 import org.eclipse.stardust.modeling.validation.util.JavaDataTypeUtils;
+import org.eclipse.xsd.XSDEnumerationFacet;
+import org.eclipse.xsd.XSDSimpleTypeDefinition;
  
 public class PrimitiveValidator implements IModelElementValidator, IBridgeObjectProvider, AccessPathEvaluationContext.Aware
 {
-   private static final String MESSAGES = Validation_Messages.MSG_PRIMITIVE_UnspecifiedType;
-   
    private AccessPathEvaluationContext context;
 
    public Issue[] validate(IModelElement element) throws ValidationException
    {
-      List<Issue> result = CollectionUtils.newList();
       String type = AttributeUtil.getAttributeValue((IExtensibleElement) element, CarnotConstants.TYPE_ATT);
       if (StringUtils.isEmpty(type))
       {
-         result.add(Issue.warning(element, MESSAGES));
+         return new Issue[] {Issue.warning(element,
+               Validation_Messages.MSG_PRIMITIVE_UnspecifiedType)};
       }
       else
       {
-         if(type.equals(Type.Enumeration.toString()))
+         if (type.equals(Type.Enumeration.toString()))
          {
-            TypeDeclarationType typeDeclaration = null;                        
-            String typeDeclarationId = AttributeUtil.getAttributeValue((IExtensibleElement) element, StructuredDataConstants.TYPE_DECLARATION_ATT);                        
-            if(!StringUtils.isEmpty(typeDeclarationId))
+            EObject ref = AttributeUtil.getIdentifiable((IExtensibleElement) element, StructuredDataConstants.TYPE_DECLARATION_ATT);
+            if (!(ref instanceof TypeDeclarationType))
             {
-               ModelType model = ModelUtils.findContainingModel(element);
-               if (model != null)
-               {
-                  TypeDeclarationsType declarations = model.getTypeDeclarations();
-                  if (declarations != null)
-                  {
-                     typeDeclaration = declarations.getTypeDeclaration(typeDeclarationId);
-                  }
-                  if(typeDeclaration == null)
-                  {
-                     result.add(Issue.error(element, MessageFormat.format(
-                           Validation_Messages.MSG_NoTypeDeclarationFound, new Object[] {typeDeclarationId}),
-                           StructuredDataConstants.TYPE_DECLARATION_ATT));
-                  }     
-               }
-            }
-            else
-            {
-               result.add(Issue.error(element, 
+               return new Issue[] {Issue.error(element, 
                      Validation_Messages.MSG_NoTypeDeclarationDefined, 
-                     StructuredDataConstants.TYPE_DECLARATION_ATT));
-            }
-
-            String defaultValue = AttributeUtil.getAttributeValue((IExtensibleElement) element, CarnotConstants.DEFAULT_VALUE_ATT);            
-            if(StringUtils.isEmpty(defaultValue))
-            {
-               result.add(Issue.error(element, 
-                     Validation_Messages.MSG_NoEnumerationDefaultValue, 
-                     CarnotConstants.DEFAULT_VALUE_ATT));
+                     StructuredDataConstants.TYPE_DECLARATION_ATT)};
             }
             else
             {
-               boolean match = false;               
-               if(typeDeclaration != null)
+               TypeDeclarationType typeDeclaration = (TypeDeclarationType) ref;                        
+
+               String defaultValue = AttributeUtil.getAttributeValue((IExtensibleElement) element, CarnotConstants.DEFAULT_VALUE_ATT);            
+               if (StringUtils.isEmpty(defaultValue))
                {
-                  XSDNamedComponent component = TypeDeclarationUtils.getSimpleType(typeDeclaration);
-                  if (component instanceof XSDSimpleTypeDefinition)
+                  return new Issue[] {Issue.error(element, 
+                        Validation_Messages.MSG_NoEnumerationDefaultValue, 
+                        CarnotConstants.DEFAULT_VALUE_ATT)};
+               }
+               else
+               {
+                  XSDSimpleTypeDefinition def = TypeDeclarationUtils.getSimpleType(typeDeclaration);
+                  if (def != null)
                   {
-                     XSDEnumerationFacet effectiveFacet = ((XSDSimpleTypeDefinition) component).getEffectiveEnumerationFacet();
-                     for(Object object : effectiveFacet.getValue())
+                     XSDEnumerationFacet effectiveFacet = def.getEffectiveEnumerationFacet();
+                     for (Object value : effectiveFacet.getValue())
                      {
-                        if(object instanceof String)
+                        if (defaultValue.equals(value))
                         {
-                           if(defaultValue.equals(object))
-                           {
-                              match = true;
-                              break;
-                           }
+                           return Issue.ISSUE_ARRAY;
                         }
                      }                     
                   }
-               }
-               if(!match)
-               {
-                  result.add(Issue.error(element, MessageFormat.format(
+                  return new Issue[] {Issue.error(element, MessageFormat.format(
                         Validation_Messages.MSG_InvalidEnumerationDefaultValue, new Object[] {defaultValue}),
-                        CarnotConstants.DEFAULT_VALUE_ATT));
-               }               
+                        CarnotConstants.DEFAULT_VALUE_ATT)};
+               }
             }
          }
+         return Issue.ISSUE_ARRAY;
       }
-      
-      return result.toArray(Issue.ISSUE_ARRAY);
    }
 
    public BridgeObject getBridgeObject(ITypedElement accessPoint, String accessPath, DirectionType direction) throws ValidationException
