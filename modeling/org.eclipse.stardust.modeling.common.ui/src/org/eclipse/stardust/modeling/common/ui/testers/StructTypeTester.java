1ca9c28946f5f50b1624cfc203442e5a49fecd26
Barry Grotjahn <barry.grotjahn@sungard.com>
2013-11-21 11:30:13 +0000

    Jira-ID: CRNT-31294 adding new filter
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ide@69063 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

---
+++ modeling/org.eclipse.stardust.modeling.common.ui/src/org/eclipse/stardust/modeling/common/ui/testers/StructTypeTester.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2011 SunGard CSA LLC and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    SunGard CSA LLC - initial API and implementation and/or initial documentation
+ *******************************************************************************/
+package org.eclipse.stardust.modeling.common.ui.testers;
+
+import org.eclipse.core.expressions.PropertyTester;
+
+import org.eclipse.stardust.engine.api.model.PredefinedConstants;
+import org.eclipse.stardust.model.xpdl.carnot.DataType;
+import org.eclipse.stardust.model.xpdl.carnot.IMetaType;
+import org.eclipse.stardust.model.xpdl.carnot.ITypedElement;
+import org.eclipse.stardust.model.xpdl.xpdl2.TypeDeclarationType;
+import org.eclipse.stardust.model.xpdl.xpdl2.util.TypeDeclarationUtils;
+
+public class StructTypeTester extends PropertyTester
+{
+   private static String COMPLEX = "complex"; //$NON-NLS-1$
+   private static String SIMPLE = "simple"; //$NON-NLS-1$
+
+   public boolean test(Object receiver, String property, Object[] args, Object expectedValue)
+   {
+      if ("structType".equals(property)) //$NON-NLS-1$
+      {
+         if(receiver instanceof DataType)
+         {
+            String structType = COMPLEX;
+            IMetaType metaType = ((ITypedElement) receiver).getMetaType();
+            String metaTypeId = metaType.getId();
+
+            if(metaTypeId.equals(PredefinedConstants.STRUCTURED_DATA))
+            {
+               TypeDeclarationType decl = TypeDeclarationUtils.findTypeDeclaration((DataType) receiver);
+               if(decl != null && TypeDeclarationUtils.isEnumeration(decl, false))
+               {
+                  structType = SIMPLE;
+               }
+            }
+
+            return expectedValue.equals(structType);
+         }
+      }
+      return false;
+   }
+}
