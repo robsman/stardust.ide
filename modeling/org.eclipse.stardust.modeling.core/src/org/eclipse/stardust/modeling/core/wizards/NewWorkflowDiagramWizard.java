23b9d4623b815dcc8d71dbec55c80f6a5a383bae
Thomas Wolfram <thomas.wolfram@sungard.com>
2014-02-21 08:09:17 +0000

    Jira-ID: CRNT-29328 Fix for Wfxml references removed from source code
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ide@70916 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b

--- modeling/org.eclipse.stardust.modeling.core/src/org/eclipse/stardust/modeling/core/wizards/NewWorkflowDiagramWizard.java
+++ modeling/org.eclipse.stardust.modeling.core/src/org/eclipse/stardust/modeling/core/wizards/NewWorkflowDiagramWizard.java
@@ -509,7 +509,6 @@ public class NewWorkflowDiagramWizard extends Wizard implements INewWizard
    private static final String[] defaultActionTypes = {
          PredefinedConstants.TRIGGER_ACTION, PredefinedConstants.MAIL_ACTION,
          PredefinedConstants.ABORT_PROCESS_ACTION,
-         PredefinedConstants.NOTIFY_OBSERVERS_ACTION,
          PredefinedConstants.COMPLETE_ACTIVITY_ACTION,
          PredefinedConstants.ACTIVATE_ACTIVITY_ACTION,
          PredefinedConstants.DELEGATE_ACTIVITY_ACTION,
