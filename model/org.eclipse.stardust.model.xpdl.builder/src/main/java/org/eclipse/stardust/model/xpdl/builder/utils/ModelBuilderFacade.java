03b64014dc4c22a5ce914f088eef896117dd4e3f
Sidharth Singh <sidharth.singh@sungard.com>
2014-02-25 15:16:53 +0100

    Jira-ID: CRNT-31624 Added code to create Primitive ENUM from DataChangeCommandHandler for Java bound Enum.
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ide@69743 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    Signed-off-by: Sidharth Singh <sidharth.singh@sungard.com>

--- model/org.eclipse.stardust.model.xpdl.builder/src/main/java/org/eclipse/stardust/model/xpdl/builder/utils/ModelBuilderFacade.java
+++ model/org.eclipse.stardust.model.xpdl.builder/src/main/java/org/eclipse/stardust/model/xpdl/builder/utils/ModelBuilderFacade.java
@@ -967,6 +967,10 @@ public class ModelBuilderFacade
       {
          type = Type.Money;
       }
+      else if (primitiveTypeID.equals(ModelerConstants.ENUM_PRIMITIVE_DATA_TYPE))
+      {
+         type = Type.Enumeration;
+      }
 
       data = newPrimitiveVariable(model).withIdAndName(dataID, dataName)
             .ofType(type)
