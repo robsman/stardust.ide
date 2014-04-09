1167fab6d22a2eb2d6e54881a5fe998817a9b775
Robert Sauer <robert.sauer@sungard.com>
2014-04-09 11:38:45 +0200

    Removing Collaborative Modeling feature from eclipse modeler (pseudo-merge of r71171 from trunk/).
    Jira-ID: CRNT-31694

--- model/org.eclipse.stardust.model.xpdl/src/org/eclipse/stardust/model/xpdl/carnot/merge/ShareUtils.java
+++ model/org.eclipse.stardust.model.xpdl/src/org/eclipse/stardust/model/xpdl/carnot/merge/ShareUtils.java
@@ -10,92 +10,29 @@
  *******************************************************************************/
 package org.eclipse.stardust.model.xpdl.carnot.merge;
 
-import org.eclipse.core.resources.IFolder;
-import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
-import org.eclipse.stardust.model.xpdl.carnot.DataType;
-import org.eclipse.stardust.model.xpdl.carnot.IMetaType;
-import org.eclipse.stardust.model.xpdl.carnot.LinkTypeType;
 import org.eclipse.stardust.model.xpdl.carnot.ModelType;
-import org.eclipse.stardust.model.xpdl.xpdl2.ExternalPackages;
-import org.eclipse.stardust.model.xpdl.xpdl2.TypeDeclarationType;
-import org.eclipse.stardust.model.xpdl.xpdl2.TypeDeclarationsType;
-import org.eclipse.team.core.RepositoryProvider;
 
+/**
+ * Fake implementation of the original ShareUtils class. About to be fully removed
+ * when merging the removal of collaborative mdoeling.
+ *
+ * @author Robert.Sauer
+ */
+public class ShareUtils {
+	public static <T> /*IFolder*/T getLockFolder(ModelType model) {
+		return null;
+	}
 
-public class ShareUtils
-{
-   public static boolean isModelShared(ModelType model)
-   {
-      IProject project = getProject(model.eResource());
-      if (project == null || !RepositoryProvider.isShared(project))
-      {
-         return false;
-      }      
-      
-      IFolder lockFolder = getLockFolder(model);
-      if (lockFolder == null || !lockFolder.exists())
-      {
-         return false;
-      }      
-      return true;
-   }   
+	public static <T> /*IProject*/T getProject(Resource eResource) {
+		return null;
+	}
 
-   public static IProject getProject(Resource eResource)
-   {
-      if (eResource != null)
-      {
-         URI eUri = eResource.getURI();
-         IResource resource = ResourcesPlugin.getWorkspace().getRoot().findMember(
-               eUri.segment(1));
-         if (resource instanceof IProject)
-         {
-            return (IProject)resource;
-         }
-         else if (resource != null)
-         {
-            return resource.getProject();
-         }
-      }
-      return null;
-   }
-   
-   public static IFolder getLockFolder(ModelType model)
-   {
-      if (model == null)
-      {
-         return null;
-      }
-      String uuid = UUIDUtils.getUUID(model);
-      if (uuid == null)
-      {
-         return null;
-      }
-      IProject project = getProject(model.eResource());
-      if (project == null)
-      {
-         return null;
-      }
-      return project.getFolder(".MODEL__" + uuid); //$NON-NLS-1$;
-   }
-   
-   public static boolean isLockableElement(Object object)
-   {
-      if (object instanceof DataType)
-      {
-         return !((DataType) object).isPredefined();
-      }         
-      return object instanceof ModelType
-            || object instanceof TypeDeclarationType
-            || object instanceof LinkTypeType
-            || object instanceof EObject 
-                  && ((EObject) object).eContainer() instanceof ModelType 
-                  && !(object instanceof TypeDeclarationsType)
-                  && !(object instanceof ExternalPackages)
-                  && !(object instanceof IMetaType);
-   }
+	public static boolean isModelShared(ModelType model) {
+		return false;
+	}
+
+	public static boolean isLockableElement(Object modelElement) {
+		return false;
+	}
 }
