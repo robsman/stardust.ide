c4781b92e09360234d74c0c8520d527a84c253f1
Sabri Bousselmi <sabri.bousselmi@sungard.com>
2014-04-24 12:12:32 +0000

    Jira-ID: CRNT-32770 Eclipse Modeler:In Generic Camel Application, CamelContextID is blank message is displayed even when Camel Context ID is present
    git-svn-id: https://svn.csa.sungard.com/repos/ipp2/product/trunk/stardust/ide@72139 8100b5e0-4d52-466c-ae9c-bdeccbdeaf6b
    
    Signed-off-by: Sabri Bousselmi <sabri.bousselmi@sungard.com>

--- modeling/org.eclipse.stardust.modeling.integration.camel/src/main/java/org/eclipse/stardust/modeling/integration/camel/CamelProducerSpringBeanApplicationInitializer.java
+++ modeling/org.eclipse.stardust.modeling.integration.camel/src/main/java/org/eclipse/stardust/modeling/integration/camel/CamelProducerSpringBeanApplicationInitializer.java
@@ -1,7 +1,10 @@
 package org.eclipse.stardust.modeling.integration.camel;
 
+import static org.eclipse.stardust.engine.extensions.camel.CamelConstants.DEFAULT_CAMEL_CONTEXT_ID;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import org.eclipse.stardust.engine.api.model.PredefinedConstants;
 import org.eclipse.stardust.engine.extensions.camel.CamelConstants;
 import org.eclipse.stardust.model.xpdl.carnot.ApplicationType;
@@ -46,6 +49,7 @@ public class CamelProducerSpringBeanApplicationInitializer extends JavaApplicati
          }
 
       }
+      result = addAttribute(result, CamelConstants.CAMEL_CONTEXT_ID_ATT, DEFAULT_CAMEL_CONTEXT_ID);
 
       return result;
    }
