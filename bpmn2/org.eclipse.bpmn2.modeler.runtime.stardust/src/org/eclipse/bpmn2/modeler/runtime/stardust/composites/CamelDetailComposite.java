e0eb7716bd15c87fabdee643912319117efcde4b
Gregor Gisler-Merz <gisler@gginfo.com>
2014-08-19 13:57:02 +0200

    Added code for support of multiple access points.
    Signed-off-by: Gregor Gisler-Merz <gisler@gginfo.com>

--- bpmn2/org.eclipse.bpmn2.modeler.runtime.stardust/src/org/eclipse/bpmn2/modeler/runtime/stardust/composites/CamelDetailComposite.java
+++ bpmn2/org.eclipse.bpmn2.modeler.runtime.stardust/src/org/eclipse/bpmn2/modeler/runtime/stardust/composites/CamelDetailComposite.java
@@ -13,25 +13,37 @@
 
 package org.eclipse.bpmn2.modeler.runtime.stardust.composites;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.bpmn2.modeler.core.merrimac.clad.AbstractBpmn2PropertySection;
 import org.eclipse.bpmn2.modeler.core.merrimac.clad.AbstractDetailComposite;
 import org.eclipse.bpmn2.modeler.core.merrimac.clad.DefaultDetailComposite;
+import org.eclipse.bpmn2.modeler.core.merrimac.clad.DefaultListComposite;
+import org.eclipse.bpmn2.modeler.core.merrimac.clad.ListCompositeColumnProvider;
+import org.eclipse.bpmn2.modeler.core.merrimac.clad.ListCompositeContentProvider;
+import org.eclipse.bpmn2.modeler.core.merrimac.clad.TableColumn;
 import org.eclipse.bpmn2.modeler.core.merrimac.dialogs.IntObjectEditor;
 import org.eclipse.bpmn2.modeler.core.merrimac.dialogs.ObjectEditor;
 import org.eclipse.bpmn2.modeler.core.merrimac.dialogs.TextObjectEditor;
 import org.eclipse.bpmn2.modeler.core.utils.ModelUtil;
+import org.eclipse.bpmn2.modeler.runtime.stardust.adapters.StardustInterfaceExtendedPropertiesAdapter;
 import org.eclipse.bpmn2.modeler.runtime.stardust.editors.AttributeTypeBooleanEditor;
 import org.eclipse.bpmn2.modeler.runtime.stardust.editors.AttributeTypeComboEditor;
 import org.eclipse.bpmn2.modeler.runtime.stardust.editors.AttributeTypeTextEditor;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.stardust.model.bpmn2.sdbpmn.SdbpmnFactory;
+import org.eclipse.stardust.model.bpmn2.sdbpmn.SdbpmnPackage;
+import org.eclipse.stardust.model.bpmn2.sdbpmn.StardustAccessPointType;
 import org.eclipse.stardust.model.bpmn2.sdbpmn.StardustApplicationType;
 import org.eclipse.stardust.model.bpmn2.sdbpmn.StardustInterfaceType;
-import org.eclipse.stardust.model.xpdl.carnot.AccessPointType;
 import org.eclipse.stardust.model.xpdl.carnot.AttributeType;
+import org.eclipse.stardust.model.xpdl.carnot.DirectionType;
 import org.eclipse.stardust.model.xpdl.carnot.CarnotWorkflowModelPackage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.ui.forms.widgets.Section;
 
 class CamelDetailComposite extends DefaultDetailComposite {
 
@@ -110,68 +122,193 @@ class CamelDetailComposite extends DefaultDetailComposite {
 		
 		Composite accessPointsSection = this.createSectionComposite(this, "Access Points");
 		
-		for (AccessPointType ap : sdApplication.getAccessPoint()) {
-			AccessPointTypeEditor ape = new AccessPointTypeEditor(this, ap);
-			Composite section  = createSectionComposite(accessPointsSection, ap.getName());
-			ape.createControl(section);
+		// create two lists, one for Input and one for Output Access Points
+		AccessPointListComposite inputParams = new AccessPointListComposite(accessPointsSection, true);
+		inputParams.bindList(sdApplication, SdbpmnPackage.eINSTANCE.getStardustApplicationType_AccessPoint1());
+		inputParams.setTitle("Inputs");
+
+		AccessPointListComposite outputParams = new AccessPointListComposite(accessPointsSection, false);
+		outputParams.bindList(sdApplication, SdbpmnPackage.eINSTANCE.getStardustApplicationType_AccessPoint1());
+		outputParams.setTitle("Outputs");
+	}
+	
+	private class AccessPointListComposite extends DefaultListComposite {
+
+		boolean isInput;
+		/**
+		 * @param parent
+		 * @param isInput
+		 */
+		public AccessPointListComposite(Composite parent, boolean isInput) {
+			super(parent, DEFAULT_STYLE);
+			this.isInput = isInput;
+		}
+		
+		@Override
+		protected int createColumnProvider(EObject object, EStructuralFeature feature) {
+			columnProvider = new ListCompositeColumnProvider(this);
+			TableColumn tc;
+			
+			// Create the table columns:
+			// the first column is the "name" feature of the StardustAccessPoint  
+			tc = columnProvider.add(object, SdbpmnPackage.eINSTANCE.getStardustAccessPointType(), CarnotWorkflowModelPackage.eINSTANCE.getIIdentifiableElement_Name());
+			tc.setEditable(false); // don't allow editing within the table
+			
+			// the second column is the value of an AttributeType contained in
+			// the StardustAccessPoint object's "attribute" list.
+			tc = new TableColumn(this, object, "") {
+
+				@Override
+				public String getHeaderText() {
+					// returns the column header text
+					return "Carnot Engine Type";
+				}
+				
+				@Override
+				public String getText(Object element) {
+					// returns the "value" feature of an AttributeType object in a list contained by StardustAccessPoint
+					// in this case, it's the AttributeType whose "name" is equal to "carnot:engine:type"
+					StardustAccessPointType ap = (StardustAccessPointType) element;
+					AttributeType at = StardustInterfaceDefinitionPropertySection.findAttributeType(ap, "carnot:engine:type");
+					if (at!=null) {
+						return at.getValue();
+					}
+					return "";
+				}
+			};
+			columnProvider.add(tc);
+			tc.setEditable(false); // don't allow editing within the table
+			
+			return 2;
+		}
+		
+		@Override
+		public ListCompositeContentProvider getContentProvider(EObject object, EStructuralFeature feature, EList<EObject>list) {
+			if (contentProvider==null) {
+				// Create a content provider for the table.
+				// A StardustApplicationType may contain any number of StardustAccessPointType objects,
+				// each of which has either an "IN" or "OUT" DirectionType.
+				// Collect only those StardustAccessPointType objects in the list that have
+				// the correct DirectionType.
+				contentProvider = new ListCompositeContentProvider(this, object, feature, list) {
+
+					@Override
+					public Object[] getElements(Object inputElement) {
+						if (inputElement instanceof List<?>) {
+							List<StardustAccessPointType> list = new ArrayList<StardustAccessPointType>();
+							for (StardustAccessPointType ap : (List<StardustAccessPointType>)inputElement) {
+								if (isInput) {
+									// we are configured for displaying only INPUT Access Points:
+									// only add  Access Points that have an "IN" DirectionType.
+									if (ap.getDirection()==DirectionType.IN_LITERAL)
+										list.add(ap);
+								}
+								else {
+									// same for "OUT" Access Points
+									if (ap.getDirection()==DirectionType.OUT_LITERAL)
+										list.add(ap);
+								}
+							}
+							return list.toArray();
+						}
+						return super.getElements(inputElement);
+					}
+				};
+			}
+			return contentProvider;
+		}
+
+		@Override
+		protected EObject addListItem(EObject object, EStructuralFeature feature) {
+			// Add a new Access Point to the list
+			StardustAccessPointType param = null;
+			param = SdbpmnFactory.eINSTANCE.createStardustAccessPointType();
+			// make sure it has the correct DirectionType
+			param.setDirection(isInput ? DirectionType.IN_LITERAL : DirectionType.OUT_LITERAL);
+			List<StardustAccessPointType> list = (List<StardustAccessPointType>)object.eGet(feature);
+			list.add(param);
+			// generate an ID for it
+			ModelUtil.setID(param);
+			// and a name based on the ID string
+			param.setName( ModelUtil.toCanonicalString(param.getId()) );
+			
+			// also create two AttributeType objects
+			AttributeType at;
+			at = StardustInterfaceExtendedPropertiesAdapter.createAttributeType("carnot:engine:type", "", null);
+			param.getAttribute().add(at);
+			
+			at = StardustInterfaceExtendedPropertiesAdapter.createAttributeType("RootElement", "", "boolean");
+			param.getAttribute().add(at);
+			
+			return param;
+		}
+
+		@Override
+		protected Object removeListItem(EObject object, EStructuralFeature feature, int index) {
+			// Determine the actual list item index by counting only the Access Points that have
+			// the correct DirectionType ("IN" or "OUT") 
+			int actualIndex = 0;
+			for (StardustAccessPointType ap : (List<StardustAccessPointType>)object.eGet(feature)) {
+				if (isInput) {
+					if (ap.getDirection()==DirectionType.IN_LITERAL) {
+						if (--index < 0)
+							break;
+					}
+				}
+				else {
+					if (ap.getDirection()==DirectionType.OUT_LITERAL) {
+						if (--index < 0)
+							break;
+					}
+				}
+				++actualIndex;
+			}
+			return super.removeListItem(object, feature, actualIndex);
+		}
+
+		@Override
+		public AbstractDetailComposite createDetailComposite(Class eClass, Composite parent, int style) {
+			AbstractDetailComposite composite = new AccessPointTypeDetailComposite(parent);
+			return composite;
 		}
 	}
 	
-	public class AccessPointTypeEditor extends Composite {
+	private class AccessPointTypeDetailComposite extends DefaultDetailComposite {
 		
 		// Yuck :-p
 		String attributeTypes[] = new String[] {
 				"carnot:engine:type",
 				"RootElement",
 		};
-		AbstractDetailComposite detailsComposite;
 		
-		AccessPointType accessPoint;
-		public AccessPointTypeEditor(AbstractDetailComposite parent, AccessPointType ap) {
+		public AccessPointTypeDetailComposite(Composite parent) {
 			super(parent, SWT.NONE);
-			accessPoint = ap;
-			detailsComposite = parent;
 		}
 		
-		public String getTitle() {
-			String direction = "";
-			switch(accessPoint.getDirection()) {
-			case IN_LITERAL:
-				direction = "Input";
-				break;
-			case INOUT_LITERAL:
-				direction = "Input/Output";
-				break;
-			case OUT_LITERAL:
-				direction = "Output";
-				break;
-			}
-			String title = ModelUtil.toCanonicalString(direction + " " + accessPoint.getName());
-			return title;
-		}
-		
-		public void createControl(Composite parent) {
-			if (parent.getParent() instanceof Section) {
-				((Section)parent.getParent()).setText(getTitle());
-			}
+		@Override
+		public void createBindings(EObject be) {
+			StardustAccessPointType accessPoint = (StardustAccessPointType) be;
+			Composite parent = getAttributesParent();
 			ObjectEditor editor;
 
-			editor = new IntObjectEditor(detailsComposite, accessPoint, CarnotWorkflowModelPackage.eINSTANCE.getIModelElement_ElementOid());
+			// Create editors/viewers for two values in the Access Point
+			editor = new IntObjectEditor(this, accessPoint, CarnotWorkflowModelPackage.eINSTANCE.getIModelElement_ElementOid());
 			editor.createControl(parent, "Element ID");
 			editor.setEditable(false);
 			
-			editor = new TextObjectEditor(detailsComposite, accessPoint, CarnotWorkflowModelPackage.eINSTANCE.getIIdentifiableElement_Name());
+			editor = new TextObjectEditor(this, accessPoint, CarnotWorkflowModelPackage.eINSTANCE.getIIdentifiableElement_Name());
 			editor.createControl(parent, "Name");
 			editor.setEditable(false);
 
+			// Create editors for the two AttributeType objects contained by the Access Point.
 			for (String name : attributeTypes) {
 				AttributeType at = StardustInterfaceDefinitionPropertySection.findAttributeType(accessPoint, name);
 				if (at!=null) {
 					if ("boolean".equals(at.getType())) {
-						editor = new AttributeTypeBooleanEditor(detailsComposite, at);
+						editor = new AttributeTypeBooleanEditor(this, at);
 					}
 					else {
-						editor = new AttributeTypeTextEditor(detailsComposite, at);
+						editor = new AttributeTypeTextEditor(this, at);
 					}
 					editor.createControl(parent, ModelUtil.toCanonicalString(name));
 				}
