65a98b12f88564e6ea95d81abe5c0b0c2b69049b
Gregor Gisler-Merz <gisler@gginfo.com>
2014-09-01 11:09:06 +0200

    Save ItemDefinition - draft - StackTrace when saving.
    Signed-off-by: Gregor Gisler-Merz <gisler@gginfo.com>

--- bpmn2/org.eclipse.bpmn2.modeler.runtime.stardust/src/org/eclipse/bpmn2/modeler/runtime/stardust/editors/StardustInterfaceSelectionObjectEditor.java
+++ bpmn2/org.eclipse.bpmn2.modeler.runtime.stardust/src/org/eclipse/bpmn2/modeler/runtime/stardust/editors/StardustInterfaceSelectionObjectEditor.java
@@ -10,16 +10,29 @@
  *******************************************************************************/
 package org.eclipse.bpmn2.modeler.runtime.stardust.editors;
 
+import java.net.MalformedURLException;
+
+import org.eclipse.bpmn2.Bpmn2Factory;
+import org.eclipse.bpmn2.Definitions;
+import org.eclipse.bpmn2.ItemDefinition;
+import org.eclipse.bpmn2.ItemKind;
 import org.eclipse.bpmn2.modeler.core.merrimac.clad.AbstractDetailComposite;
 import org.eclipse.bpmn2.modeler.core.merrimac.dialogs.TextAndButtonObjectEditor;
+import org.eclipse.bpmn2.modeler.core.utils.ModelUtil;
+import org.eclipse.bpmn2.modeler.runtime.stardust.utils.IntrinsicJavaAccesspointInfo;
 import org.eclipse.bpmn2.modeler.runtime.stardust.utils.Messages;
 import org.eclipse.bpmn2.modeler.runtime.stardust.utils.StardustInterfaceSelectionDialog;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
+import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.emf.transaction.util.TransactionUtil;
 import org.eclipse.jdt.core.IMethod;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 
 public class StardustInterfaceSelectionObjectEditor extends TextAndButtonObjectEditor {
 
@@ -47,10 +60,34 @@ public class StardustInterfaceSelectionObjectEditor extends TextAndButtonObjectE
         System.out.println("Selected Type Full Qulified Name:" + selectedType.getFullyQualifiedName().toString());
         System.out.println("Selected IType:" + dialog.getIType().toString());
         System.out.println("Selected Methods:" + dialog.getIMethods().toString());
-		IMethod[] selectedMethods = dialog.getIMethods();
+		final IMethod[] selectedMethods = dialog.getIMethods();
+		if (selectedMethods.length <= 0) return;
 		for (int i=0; i<dialog.getIMethods().length; ++i)
 			System.out.println("selectedMethod " + i + ": " + selectedMethods[i].toString() );
-
-        
+				
+		Display.getDefault().asyncExec( new Runnable() {
+			@Override
+			public void run() {
+				TransactionalEditingDomain editingDomain = TransactionUtil.getEditingDomain(object.eResource());
+				editingDomain.getCommandStack().execute(new RecordingCommand(editingDomain) {
+					@Override
+					protected void doExecute() {
+						Definitions definitions = ModelUtil.getDefinitions(object.eResource());
+						ItemDefinition itemDef = Bpmn2Factory.eINSTANCE.createItemDefinition();
+						itemDef.setItemKind(ItemKind.INFORMATION);
+						String generateID = ModelUtil.generateID(itemDef, object.eResource(), object.eClass().getName());
+						itemDef.setId(generateID);
+						try {
+							IntrinsicJavaAccesspointInfo.addAccessPointItemDefinitionSchema(selectedMethods[0], itemDef);
+						} catch (ClassNotFoundException | NoSuchMethodException
+								| SecurityException | MalformedURLException
+								| CoreException e) {
+							e.printStackTrace();
+						}
+						definitions.getRootElements().add(itemDef);
+					}
+				});
+			}
+		});
 	}
 }
--- bpmn2/org.eclipse.bpmn2.modeler.runtime.stardust/src/org/eclipse/bpmn2/modeler/runtime/stardust/utils/Messages.java
+++ bpmn2/org.eclipse.bpmn2.modeler.runtime.stardust/src/org/eclipse/bpmn2/modeler/runtime/stardust/utils/Messages.java
@@ -16,6 +16,7 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.bpmn2.modeler.runtime.stardust.utils.messages"; //$NON-NLS-1$
 	public static String StardustInterfaceSelectionObjectEditor_Browse;
 	public static String StardustInterfaceSelectionDialog_Available_Methods;	
+	public static String StardustInterfaceDetailsSection_Import_Action;
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
