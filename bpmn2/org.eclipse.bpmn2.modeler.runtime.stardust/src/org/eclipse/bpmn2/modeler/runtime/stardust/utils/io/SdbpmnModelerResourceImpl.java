907ec9cabdf827af04539d6220d5b79a8bdd0714
Bob Brodt <bbrodt@redhat.com>
2014-10-13 15:30:15 -0600

    Oops, wrong import for EmbeddedXSDResourceImpl!
    Signed-off-by: Bob Brodt <bbrodt@redhat.com>

--- bpmn2/org.eclipse.bpmn2.modeler.runtime.stardust/src/org/eclipse/bpmn2/modeler/runtime/stardust/utils/io/SdbpmnModelerResourceImpl.java
+++ bpmn2/org.eclipse.bpmn2.modeler.runtime.stardust/src/org/eclipse/bpmn2/modeler/runtime/stardust/utils/io/SdbpmnModelerResourceImpl.java
@@ -4,14 +4,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
-import org.eclipse.bpmn2.modeler.core.EmbeddedXSDResourceImpl;
 import org.eclipse.bpmn2.modeler.core.model.Bpmn2ModelerResourceImpl;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.URIConverter;
-import org.eclipse.emf.ecore.util.FeatureMap;
 import org.eclipse.emf.ecore.xmi.XMLHelper;
 import org.eclipse.emf.ecore.xmi.XMLLoad;
 import org.eclipse.emf.ecore.xmi.XMLResource;
