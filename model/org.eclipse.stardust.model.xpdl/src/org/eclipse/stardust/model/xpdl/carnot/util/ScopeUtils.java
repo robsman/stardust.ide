c7d8524a36501e74b18556effb351fdcc60d8944
Stephan Born <stephan.born@sungard.com>
2015-02-12 10:23:37 +0100

    [CRNT-35480]: Cannot Use Enumeration-typed Data as Department Criterion: allowed primitive enums + don't longer force dataPath for SDT.
    Bug: CRNT-35480

--- model/org.eclipse.stardust.model.xpdl/src/org/eclipse/stardust/model/xpdl/carnot/util/ScopeUtils.java
+++ model/org.eclipse.stardust.model.xpdl/src/org/eclipse/stardust/model/xpdl/carnot/util/ScopeUtils.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2011 SunGard CSA LLC and others.
+ * Copyright (c) 2011, 2015 SunGard CSA LLC and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -28,14 +28,14 @@ import org.eclipse.stardust.model.xpdl.carnot.RoleType;
 import org.eclipse.stardust.model.xpdl.xpdl2.TypeDeclarationType;
 
 public class ScopeUtils
-{  
+{
    public static boolean isValidScopeData(IExtensibleElement extensible, String dataPath, DataType data)
-   {          
+   {
       boolean valid = true;
 
       ModelType model = ModelUtils.findContainingModel(extensible);
-      
-      String dataId = AttributeUtil.getAttributeValue(extensible, PredefinedConstants.BINDING_DATA_ID_ATT);               
+
+      String dataId = AttributeUtil.getAttributeValue(extensible, PredefinedConstants.BINDING_DATA_ID_ATT);
       if (StringUtils.isEmpty(dataId) && data == null)
       {
          valid = false;
@@ -46,47 +46,40 @@ public class ScopeUtils
          {
             data = (DataType) ModelUtils.findElementById(model.getData(), dataId);
          }
-         
+
          if (data != null)
-         {         
+         {
             if (data.getType().getId().equals(PredefinedConstants.STRUCTURED_DATA))
-            {               
+            {
                if (dataPath == null)
                {
                   dataPath = AttributeUtil.getAttributeValue(extensible, PredefinedConstants.BINDING_DATA_PATH_ATT);
                }
-               
-               if (StringUtils.isEmpty(dataPath))
+
+               TypeDeclarationType type = StructuredTypeUtils.getTypeDeclaration(data);
+               if (type == null)
                {
                   valid = false;
                }
                else
                {
-                  TypeDeclarationType type = StructuredTypeUtils.getTypeDeclaration(data);
-                  if (type == null)
+                  IXPathMap xPathMap = StructuredTypeUtils.getXPathMap(data);
+                  if (StructuredDataXPathUtils.returnSinglePrimitiveType(dataPath, xPathMap) != BigData.STRING)
                   {
                      valid = false;
                   }
-                  else
-                  {
-                     IXPathMap xPathMap = StructuredTypeUtils.getXPathMap(data); 
-                     if (StructuredDataXPathUtils.returnSinglePrimitiveType(dataPath, xPathMap) != BigData.STRING)
-                     {
-                        valid = false;     
-                     }
-                  }                  
                }
             }
             else if (data.getType().getId().equals(PredefinedConstants.PRIMITIVE_DATA))
             {
                String type = AttributeUtil.getAttributeValue((IExtensibleElement) data, CarnotConstants.TYPE_ATT);
-               if (Type.String.equals(type))
-               {                  
+               if (Type.String.equals(type) || Type.Enumeration.equals(type))
+               {
                   if (dataPath == null)
-                  {                  
+                  {
                      dataPath = AttributeUtil.getAttributeValue(extensible, PredefinedConstants.BINDING_DATA_PATH_ATT);
                   }
-                  
+
                   if (!StringUtils.isEmpty(dataPath))
                   {
                      valid = false;
@@ -108,41 +101,41 @@ public class ScopeUtils
          }
       }
       return valid;
-   }   
-   
+   }
+
    public static boolean isValidScopedParticipantForManualTrigger(IModelParticipant participant)
-   {          
+   {
       OrganizationType organization = ScopeUtils.findScopedOrganizationForParticipant(participant);
       return isValidScopeData(organization, null, null);
    }
-   
+
    public static HashSet<IModelParticipant> findScopedParticipants(ModelType model)
    {
       HashSet<IModelParticipant> scoped = new HashSet<IModelParticipant>();
-      
+
       for(OrganizationType organization : model.getOrganization())
       {
          if(AttributeUtil.getBooleanValue((IExtensibleElement) organization, PredefinedConstants.BINDING_ATT))
          {
-            collectChildren(organization, scoped);            
+            collectChildren(organization, scoped);
          }
-      }      
+      }
       return scoped;
    }
-   
+
    public static OrganizationType findScopedOrganizationForParticipant(IModelParticipant element)
-   {          
+   {
       if(element instanceof OrganizationType)
       {
          if(AttributeUtil.getBooleanValue((IExtensibleElement) element, PredefinedConstants.BINDING_ATT))
          {
             return (OrganizationType) element;
-         }         
+         }
       }
-      
+
       ModelType model = ModelUtils.findContainingModel(element);
-      
-      OrganizationType parent = null;      
+
+      OrganizationType parent = null;
       for(OrganizationType organization : model.getOrganization())
       {
          RoleType teamLead = organization.getTeamLead();
@@ -160,28 +153,28 @@ public class ScopeUtils
                {
                   parent = organization;
                   break;
-               }               
+               }
             }
-         }            
-      }               
-      
+         }
+      }
+
       if(parent == null)
       {
          return null;
       }
-      
+
       if(AttributeUtil.getBooleanValue((IExtensibleElement) parent, PredefinedConstants.BINDING_ATT))
       {
          return parent;
-      }         
-      
+      }
+
       return findScopedParent(parent);
-   }   
-   
+   }
+
    private static OrganizationType findScopedParent(OrganizationType element)
    {
       ModelType model = ModelUtils.findContainingModel(element);
-      
+
       for(OrganizationType organization : model.getOrganization())
       {
          for(ParticipantType participant : organization.getParticipant())
@@ -191,29 +184,29 @@ public class ScopeUtils
             {
                if(childParticipant.equals(element))
                {
-                  
+
             	  if(AttributeUtil.getBooleanValue((IExtensibleElement) organization, PredefinedConstants.BINDING_ATT))
                   {
                      return organization;
                   }
-                  return findScopedParent(organization);                  
+                  return findScopedParent(organization);
                }
-            }         
-         }         
-      }      
+            }
+         }
+      }
       return null;
    }
 
    private static void collectChildren(OrganizationType organization, HashSet<IModelParticipant> scoped)
-   {      
+   {
       scoped.add(organization);
-      
+
       RoleType teamLead = organization.getTeamLead();
       if(teamLead != null)
       {
          scoped.add(teamLead);
       }
-      
+
       for(ParticipantType participant : organization.getParticipant())
       {
          IModelParticipant childParticipant = participant.getParticipant();
@@ -222,7 +215,7 @@ public class ScopeUtils
             if(!scoped.contains(childParticipant))
             {
                collectChildren((OrganizationType) childParticipant, scoped);
-            }            
+            }
          }
          scoped.add(childParticipant);
       }
