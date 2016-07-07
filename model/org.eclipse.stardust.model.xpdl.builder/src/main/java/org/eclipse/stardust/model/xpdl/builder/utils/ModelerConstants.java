1d9f2822d663564ff9f63d6652a4b8333ad1f940
Barry Grotjahn <barry.grotjahn@sungard.com>
2016-07-07 14:45:59 +0200

    CRNT-41066 new constants added

--- model/org.eclipse.stardust.model.xpdl.builder/src/main/java/org/eclipse/stardust/model/xpdl/builder/utils/ModelerConstants.java
+++ model/org.eclipse.stardust.model.xpdl.builder/src/main/java/org/eclipse/stardust/model/xpdl/builder/utils/ModelerConstants.java
@@ -19,6 +19,10 @@ public class ModelerConstants
    public static final String DATAMAPPINGS_PROPERTY = "dataMappings";
    public static final String DATAMAPPING_IN = "IN";
    public static final String DATAMAPPING_OUT = "OUT";
+   
+   public static final String DATAMAPPING_CONSTANT_TYPE = "dataMappingConstantType";
+   public static final String DATAMAPPING_CONSTANT_VALUE = "dataMappingConstantValue";
+      
    public static final String DATA_NAME_PROPERTY = "dataName";
    public static final String DATA_FULL_ID_PROPERTY = "dataFullId";
    public static final String DATA_PROPERTY = "data";
